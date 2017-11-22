7a15459e723476c1347531d0d1bae3bf19d3ca72
Mateusz <mateuszhotowy@gmail.com>
2017-11-22 23:17:46 +0100

    Commit 2 - dodanie nowych linii

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -4,6 +4,6 @@ public class Zadanie {
     public static void main(String[] args) {
         System.out.println("Uczę");
         System.out.println("się");
-        System.out.println("programowania");
+        System.out.println("programowania \ni \ncommitowania");
     }
 }
